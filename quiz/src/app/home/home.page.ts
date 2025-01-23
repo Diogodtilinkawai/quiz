dddec8ededaa95b445fc63acaf6dd731c26102ab
Donvicio <10143@portalcedi.edu.mx>
2025-01-23 08:30:06 -0600

    quiz sin css

--- quiz/src/app/home/home.page.ts
+++ quiz/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,26 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  nombre = new FormControl('');
+  fechainicio = new FormControl('');
+  fechafin = new FormControl('');
+  ubicacion = new FormControl('');
+  encargado = new FormControl('');
+  numero = new FormControl('');
 
-  constructor() {}
+  items: any = [];
+
+  agregar() {
+    if (this.nombre.value && this.fechainicio.value && this.fechafin.value && this.ubicacion.value && this.encargado.value && this.numero.value) {
+      this.items.push({ nombre: this.nombre.value, fecha1: this.fechainicio.value, fecha2: this.fechafin.value, ubicacion: this.ubicacion.value, encargado: this.encargado.value, numero: this.numero.value });
+      this.nombre.setValue('');
+      this.fechainicio.setValue('');
+      this.fechafin.setValue('');
+      this.ubicacion.setValue('');
+      this.encargado.setValue('');
+      this.numero.setValue('');
+    }
+  }
+  constructor() { }
 
 }
