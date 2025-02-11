0d30660e03aabea3539f257206091d13505b3a02
Donvicio <10143@portalcedi.edu.mx>
2025-02-11 09:05:30 -0600

    navegacion y formulario

---
+++ quiz/src/app/examen.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExamenService {
+  // private apiurl = 'https://qapi.vercel.app/api/random';
+  // constructor(private http: HttpClient) { }
+  // chistes() {
+  //   return this.http.get(this.apiurl);
+  // }
+}
--- quiz/src/app/home/home.page.ts
+++ quiz/src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ExamenService } from '../examen.service';
 
 @Component({
   selector: 'app-home',
@@ -9,28 +11,37 @@ import { FormControl } from '@angular/forms';
 })
 export class HomePage {
   nombre = new FormControl('');
-  fechainicio = new FormControl('');
-  fechafin = new FormControl('');
-  ubicacion = new FormControl('');
-  encargado = new FormControl('');
-  numero = new FormControl('');
+  ano = new FormControl('');
+  productora = new FormControl('');
+  casa = new FormControl('');
+  prota = new FormControl('');
+
 
   items: any = [];
-
+  chistes: any;
   agregar() {
-    if (this.nombre.value && this.fechainicio.value && this.fechafin.value && this.ubicacion.value && this.encargado.value && this.numero.value) {
-      this.items.push({ nombre: this.nombre.value, fecha1: this.fechainicio.value, fecha2: this.fechafin.value, ubicacion: this.ubicacion.value, encargado: this.encargado.value, numero: this.numero.value });
+    if (this.nombre.value && this.ano.value && this.productora.value && this.casa.value && this.prota.value ) {
+      this.items.push({ nombre: this.nombre.value, fecha1: this.ano.value, fecha2: this.productora.value, casa: this.casa.value, prota: this.prota.value});
       this.nombre.setValue('');
-      this.fechainicio.setValue('');
-      this.fechafin.setValue('');
-      this.ubicacion.setValue('');
-      this.encargado.setValue('');
-      this.numero.setValue('');
+      this.ano.setValue('');
+      this.productora.setValue('');
+      this.casa.setValue('');
+      this.prota.setValue('');
+
     }
     else {
       alert('Llene todos los campos');
     }
   }
-  constructor() { }
-
+  redirect()
+  {
+    this.router.navigate(['/formulario'])
+  }
+  constructor(private router:Router, private examenservice:ExamenService) { }
+  // ngOnInit() {
+  //   this.examenservice.chistes().subscribe((data) => {
+  //     this.chistes = data;
+  //     console.log(this.chistes);
+  //   });
+  // }
 }
